9b1bd57c6fcd2cb227469256b89ca5c542a3dc33
Sebastian Carrasco <scarrasco@trytoku.com>
2025-12-24 00:07:39 -0300

    ✅ add integration tests

--- tests/helpers/auth.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateInvalidToken = () => jwt.sign({ sub: 'fake-id' }, 'wrong-secret', { algorithm: 'HS256' });
-
-module.exports = { generateInvalidToken };
